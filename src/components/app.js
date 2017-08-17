5508f06def5ca7223ed9b066fcf8fddb13c43b86
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-08-17 10:13:09 -0400

    initial css styling

--- src/components/app.js
+++ src/components/app.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Component } from 'react';
 
 // Components
+import Header from './header.js';
 import NoteList from '../containers/note-list';
 import NoteArea from '../containers/note-area';
 
@@ -9,6 +10,7 @@ export default class App extends Component {
 	render() {
 		return (
 			<div>
+				<Header />
 				<NoteList />
 				<NoteArea />
 			</div>
--- src/components/header.js
+++ src/components/header.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 const Header = () => {
 	return (
-		<div className="header">
-			<h1> NoteBook </h1>
-			<button>New Note</button>
+		<div className="header row">
+			<h1 className="col-sm-6"> NoteBook </h1>
+			<div className="col-sm-6">
+				<button>New Note</button>
+				<button>Delete Note</button>
+			</div>
 		</div>
 	);
 };
