6c100abf85f7f5602dd3cf4bb5ab4eda26e0b4d8
Deinyefa Eporwei <df.eporwei@gmail.com>
2017-08-21 14:25:36 -0400

    bug fix: new selected note becomes provious note when a not is deleted, when all notes have been deleted, a fresh note is automatically created

--- src/reducers/reducer_notes.js
+++ src/reducers/reducer_notes.js
@@ -32,13 +32,29 @@ export default function(
 			};
 
 		case 'REMOVE_NOTE':
-			state.notelist.splice(
-				state.notelist.findIndex(element => element.id == state.selected.id),
-				1
+			let index = state.notelist.findIndex(
+				element => element.id == state.selected.id
 			);
+			state.notelist.splice(index, 1);
+
+			let newSelected;
+			if (state.notelist.length == 0) {
+				nextID = 1;
+				return {
+					...state,
+					notelist: [...state.notelist, { title: '', id: nextID }],
+					selected: { ...state.selected, title: '' }
+				};
+			} else if (index < state.notelist.length)
+				newSelected = state.notelist[index];
+			else {
+				newSelected = state.notelist[index - 1];
+			}
+
 			return {
 				...state,
-				notelist: [...state.notelist]
+				notelist: [...state.notelist],
+				selected: newSelected
 			};
 	}
 	return state;
